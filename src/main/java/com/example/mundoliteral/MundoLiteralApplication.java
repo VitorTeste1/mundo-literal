cdd9ab5666c1e55e28510371381488e715f5be42
VitorVieira <vitorhvvieira@gmail.com>
2023-09-11 22:55:53 -0300

    Adicionando arquivos
    Adicionando arquivos do código

---
+++ src/main/java/com/example/mundoliteral/MundoLiteralApplication.java
@@ -0,0 +1,13 @@
+package com.example.mundoliteral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoLiteralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoLiteralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mundoliteral/MundoLiteralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mundoliteral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoLiteralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
